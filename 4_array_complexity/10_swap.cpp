539812d7b74bf6f931386a63f2285004fd0ed802
hritviik715 <hritvikmohan7151221@gmail.com>
2024-05-23 21:26:53 +0530

    23MAY

--- 4_array_complexity/10_swap.cpp
+++ 4_array_complexity/10_swap.cpp
@@ -3,15 +3,46 @@ using namespace std;
 int main()
 {
 
-    int a=5;
-    int b=7;
 
-    a = a+b;
-    b=a-b;
-    a=a-b;
-    cout<<a<<" "<<b;
+    //1. swap inbuilt fxn
+    // int a =5;
+    // int b=7;
+    // swap(a,b);
+    // cout<<a<<" "<<b<<endl;
+
+
+
+    //2. temp variable
+    // int a =5;
+    // int b=7;
+    // int temp = a;
+    // a=b;
+    // b=temp;
+    // cout<<a<<" "<<b<<endl;
+
+
+
+
+
+    //3.arithmatic method
+    // int a=5;
+    // int b=7;
+
+    // a = a+b;
+    // b=a-b;
+    // a=a-b;
+    // cout<<a<<" "<<b<<endl;
        
        
        
+    //4. xor bitwise method
+    int a =5;
+    int b=7;
+    a=a^b;
+    b= a^b;
+    a=a^b;
+    cout<<a<<" "<<b<<endl;
+
+
     return 0;
 }
---
+++ 4_array_complexity/3_fill_part_2.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    
+
+
+    int a[10];
+    //imp->
+    // fill(a , a+5 ,67);//67 will be filled [a,a+5)
+    // fill(a+5 , a+10 , 100);//100 will be filled [a+5 , a+10)
+
+
+    //imp->
+    // fill(a[0] , a+5 ,67);//this wont work
+    // fill(a[0]+5 , a+10 , 100);//this wont work
+
+    //imp->
+    fill(&a[0] , a+5 ,67);
+    fill(&a[0]+5 , a+10 , 100);
+
+    for(int i=0 ; i<10 ; i++){
+        cout<<i <<" --> "<<a[i]<<endl; 
+    }
+       
+       
+       
+    return 0;
+}   
